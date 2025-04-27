38d12135843331c5ac45bff62dd932eb9c0ad161
AKToran <aktoran60@gmail.com>
2025-04-27 21:22:47 +0600

    get user info from google

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,17 +1,18 @@
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import React from 'react';
+import React, { useState } from 'react';
 import { auth } from '../../firebase/firebase.init';
 
 const Login = () => {
+  const [user, setUser] = useState(null);
 
   const provider = new GoogleAuthProvider;
-
 
   const handleGoogleSignIn = () =>{
     alert("d");
     signInWithPopup(auth, provider)
     .then(result =>{
-      console.log(result);
+      const user = result.user;
+      setUser(user);
     })
     .catch(error =>{
       console.log(error);
@@ -22,6 +23,13 @@ const Login = () => {
     <div className='text-center'>
       <h1>Please Login</h1>
       <button onClick={handleGoogleSignIn} className='border-2 hover:cursor-pointer rounded-xl p-2 bg-green-500'>Sign In with Google</button>
+      {
+        user && <div>
+          <h1>{user.displayName}</h1>
+          <h3>{user.email}</h3>
+          <img src={user.photoURL} alt="" />
+        </div>
+      }
     </div>
   );
 };
